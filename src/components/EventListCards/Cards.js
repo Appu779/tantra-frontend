1da302b4b071f7694d0e7c8f723c0fa95448467d
Vyshnav Sreeshan <vyshnav.sreeshan05@gmail.com>
2023-11-20 01:54:44 +0530

    reg close button

--- src/components/EventListCards/Cards.js
+++ src/components/EventListCards/Cards.js
@@ -33,8 +33,8 @@ function Cards(props) {
           </p>
         ) : (
           <p className="text-center">
-            <button className="bg-white font-mono text-black px-2 py-2 rounded-full">
-              Register Event
+            <button style={{cursor: "no-drop"}} className="bg-white font-mono text-black px-2 py-2 rounded-full">
+               Closed
             </button>
           </p>
           // null
