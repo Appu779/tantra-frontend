9169345fbda06d52a2bd27c67b89d7885e4cc21a
Ashik Jhonson <ashikjhonson@gmail.com>
2023-11-22 19:20:41 +0530

    Event cards

--- src/components/EventListCards/Cards.js
+++ src/components/EventListCards/Cards.js
@@ -1,31 +1,52 @@
 import React from "react";
 // import "./style.css";
 import ViewDetails from "../popup/ViewDetails";
+import { Call, LocationOn, Person, Timer } from "@material-ui/icons";
 import { useNavigate } from "react-router-dom";
 
 function Cards(props) {
   let navigate = useNavigate();
   let eventData = props.data;
+
+  console.log(typeof fee);
   return (
-    <div className="w-72 p-1 rounded-2xl overflow-hidden shadow-lg bg-gray-200 mx-auto">
+    <div className="w-72 p-1 rounded-2xl overflow-hidden shadow-lg bg-gray-200 mx-auto ">
       {/*change angle of transition, 3d effect of turning the card here, in perspective */}
       <div class="group h-96 w-50 [perspective:5000px]">
         {/*change speed of transition here, in duration */}
-        <div class="relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
+        <div class="relative h-full w-full rounded-xl shadow-xl transition-all  [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
           <div class="absolute inset-0">
-            <div className="h-44 w-30 rounded-2xl bg-black"></div>
-            <div className="pt-5">
-              <div className="text-center font-bold font-mono text-3xl mb-2 h-16">
-                {props.data.name}
+            <div className="h-44 w-30 rounded-2xl bg-black py-16">
+              <div className="text-center text-white font-bold font-serif text-3xl mb-2 h-16">
+                {eventData.name}
               </div>
-              <p className="text-center font-mono">{props.data.event_type}</p>
-              <p className="text-center font-mono">
-                {props.data.fee !== "" ? `Reg Fee: ${props.data.fee}` : "‎ "}
-              </p>
             </div>
-            <div className="px-0 pt-4 pb-2">
-              <p className="text-center">
-                <button className="bg-white font-mono text-black px-4 py-2 rounded-full">
+            <div className="flex flex-col px-3 font-mono ">
+              <p className="flex flex-row justify-between pt-3">
+                <span className="bg-gray-800 text-white px-2 py-1 rounded-lg grow mr-3 text-center">
+                  {props.data.event_type}
+                </span>
+                <span className="border border-gray-900 bg-gray-300 px-1 py-1 rounded-lg">
+                  Reg Fee{" "}
+                  <span className="bg-gray-800 text-white px-2 py-0.5 rounded-md">
+                    {eventData.fee}
+                  </span>
+                </span>
+              </p>
+              <p className="text-sm flex pt-4 min-h-[56px]">
+                <LocationOn />
+                <span className="pl-2">{eventData.venue}</span>
+              </p>
+              <p className="text-sm self-start">
+                <Timer />
+                <span className="pl-2 align-bottom">{eventData.time}</span>
+              </p>
+              <p className="text-sm self-start pt-2 pb-3">
+                <Person />
+                <span className="pl-2 align-bottom ">{eventData.student1}</span>
+              </p>
+              <p className="text-center self-center">
+                <button className="bg-gray-900 text-white px-4 py-1 shadow-xl rounded-lg">
                   View Event
                 </button>
               </p>
@@ -40,20 +61,22 @@ function Cards(props) {
                 <p class="text-base ">{props.data.description}</p>
               </div>
 
-              {eventData.reg ? (
+              <div className="flex justify-center gap-5 w-full">
                 <button
-                  className="mt-3 rounded-xl bg-neutral-800 px-3 py-1 text-sm hover:bg-neutral-900 align-bottom"
-                  onClick={() => navigate("/register", { state: eventData })}
+                  className="text-gray-900  bg-gray-300 font-mono rounded-full px-4 py-1.5 text-center"
+                  onClick={() => {
+                    if (eventData.reg)
+                      navigate("/register", { state: eventData });
+                  }}
                 >
-                  Register Event
+                  {eventData.reg ? "Register Event" : "Closed"}
                 </button>
-              ) : (
-                <button
-                  className="mt-3 rounded-md bg-neutral-800 px-2 py-1 text-sm hover:bg-neutral-900 cursor-not-allowed"
-                >
-                  Closed
-                </button>
-              )}
+                <p className="text-sm h-10 w-10 bg-gray-800 rounded-full py-2 text-green-500 ">
+                  <a href={`tel:${eventData.s1_phone}`}>
+                    <Call />
+                  </a>
+                </p>
+              </div>
             </div>
           </div>
         </div>
