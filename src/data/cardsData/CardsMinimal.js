cd909b06133115094a8664805ee6dff2b3f57104
Ashik Jhonson <ashikjhonson@gmail.com>
2023-11-23 15:28:19 +0530

    Commented EEE, ECE & CSBS

--- src/data/cardsData/CardsMinimal.js
+++ src/data/cardsData/CardsMinimal.js
@@ -27,24 +27,24 @@ const CardsData = [
     text: "Events conducted by Department of Civil Engineering",
     explore: "/ce-events",
   },
-  {
-    imgsrc: ECE,
-    title: "Electronics & Communication",
-    text: "Events conducted by Department of Electronics and Communication Engineering",
-    explore: "/ece-events",
-  },
+  // {
+  //   imgsrc: ECE,
+  //   title: "Electronics & Communication",
+  //   text: "Events conducted by Department of Electronics and Communication Engineering",
+  //   explore: "/ece-events",
+  // },
   {
     imgsrc: AEI,
     title: "Applied Electronics & Instrumentation",
     text: "Events conducted by AEI Department",
     explore: "/aei-events",
   },
-  {
-    imgsrc: EEE,
-    title: "Electrical & Electronics",
-    text: "Events conducted by EEE Department",
-    explore: "/eee-events",
-  },
+  // {
+  //   imgsrc: EEE,
+  //   title: "Electrical & Electronics",
+  //   text: "Events conducted by EEE Department",
+  //   explore: "/eee-events",
+  // },
   {
     imgsrc: ADS,
     title: "Artificial Intelligence & Data Science",
@@ -57,12 +57,12 @@ const CardsData = [
     text: "Events conducted by Department of Computer Science And Design",
     explore: "/csd-events",
   },
-  {
-    imgsrc: ADS,
-    title: "Computer Science And Business",
-    text: "Events conducted by Department of Computer Science And Business",
-    explore: "/csbs-events",
-  },
+  // {
+  //   imgsrc: ADS,
+  //   title: "Computer Science And Business",
+  //   text: "Events conducted by Department of Computer Science And Business",
+  //   explore: "/csbs-events",
+  // },
   {
     imgsrc: ADS,
     title: "Computer Science And Cyber Security",
