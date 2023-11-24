cd92c37445e52f80b37fc4050a6034f309711146
Ashik Jhonson <ashikjhonson@gmail.com>
2023-11-24 21:22:27 +0530

    Removed bg-select on event section click

--- src/components/cards/EventCard.js
+++ src/components/cards/EventCard.js
@@ -8,8 +8,9 @@ function EventCard(props) {
 
   return (
     <div
-      className="cursor-pointer border-b last:border-b-0 border-gray-400 py-10"
+      className="sm:cursor-pointer border-b last:border-b-0 border-gray-400 py-10 "
       onClick={() => navigate(props.explore)}
+      aria-selected="false"
     >
       <h2 className="font-mono font-bold text-gray-400 text-3xl sm:text-5xl sm:w-3/4 whitespace-pre-wrap hover:scale-105 hover:pl-3 hover:w-4/5 hover:text-gray-200">
         {props.title}
